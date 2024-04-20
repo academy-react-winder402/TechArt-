6507a8cdb7b8cd1b1c680eed430a576c6b64b028
M.Hashemi <malihe.hashemi2020@gmail.com>
2024-04-20 22:36:52 +0330

    card component error fixed

--- src/Components/Common/Card.jsx
+++ src/Components/Common/Card.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const CardComponent = ({ name, image, description }) => {
+const CardComponent = ({ title, image, description }) => {
   return (
     <div className="max-w-xs rounded overflow-hidden shadow-lg">
-      <img className="w-full" src={image} alt={name} />
+      <img className="w-full" src={image} alt={title} />
       <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{name}</div>
+        <div className="font-bold text-xl mb-2">{title}</div>
         <p className="text-gray-700 text-base">{description}</p>
       </div>
     </div>
